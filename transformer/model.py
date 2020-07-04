53bed83a460492a83823637cffe5249e8b24c694
KimSooHwan <sh951011@gmail.com>
2020-07-04 21:16:39 +0900

    Construct whole structure

---
+++ transformer/model.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+
+
+class Transformer(nn.Module):
+    def __init__(self):
+        super(Transformer, self).__init__()
+
+    def forward(self):
+        pass
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+    def forward(self):
+        pass
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+    def forward(self):
+        pass
+
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self):
+        super(ScaledDotProductAttention, self).__init__()
+
+    def forward(self):
+        pass
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self):
+        super(MultiHeadAttention, self).__init__()
+
+    def forward(self):
+        pass
+
+
+class LayerNorm(nn.Module):
+    def __init__(self):
+        super(LayerNorm, self).__init__()
+
+    def forward(self):
+        pass
+
+
+class PoswiseFeedForwardNet(nn.Module):
+    def __init__(self):
+        super(PoswiseFeedForwardNet, self).__init__()
+
+    def forward(self):
+        pass
+
